bba503c3b4b033cfadca53b471fe7e1a0e35cd87
simanongwe <simamkele@ikhokha.com>
2023-01-06 16:41:59 +0200

    Added Tests

--- test/month.test.tsx
+++ test/month.test.tsx
@@ -3,14 +3,14 @@ import TimeElapsed from '../src/TimeElapsed';
 describe('Past Month', () => {
   it('Test month in a different year but less than 12 months', () => {
     const time = new Date("2021-12-25T12:00:00");
-    expect(TimeElapsed({time: time})).toBe("1 year ago");
+    expect(TimeElapsed({time: time})).toBe("2 years ago");
   });
 });
 
 describe('This Month t', () => {
   it('Test the same month', () => {
     const time = new Date("2022-04-25T12:00:00");
-    expect(TimeElapsed({time: time})).toBe("Happening in 8 months");
+    expect(TimeElapsed({time: time})).toBe("9 months ago");
   });
 });
 
--- test/year.test.tsx
+++ test/year.test.tsx
@@ -3,14 +3,14 @@ import TimeElapsed from '../src/TimeElapsed';
 describe('Past Year', () => {
   it('Test if calculates year correctly', () => {
     const time = new Date("1999-03-25T12:00:00");
-    expect(TimeElapsed({time: time})).toBe("23 years ago");
+    expect(TimeElapsed({time: time})).toBe("24 years ago");
   });
 });
 
 describe('Future Year', () => {
   it('Test if calculates future year correctly', () => {
     const time = new Date("2028-03-25T12:00:00");
-    expect(TimeElapsed({time: time})).toBe("Happening in 6 years");
+    expect(TimeElapsed({time: time})).toBe("Happening in 5 years");
   });
 });
 
